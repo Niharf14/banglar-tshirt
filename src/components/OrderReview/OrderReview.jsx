0045c6f75bf451c0ad6d8d178dadf2faa4aea040
Niharf14 <niharf14@gmail.com>
2023-04-09 15:47:17 +0530

    initial data load

---
+++ src/components/OrderReview/OrderReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderReview = () => {
+    return (
+        <div>
+            <h3>This is a order review page</h3>
+        </div>
+    );
+};
+
+export default OrderReview;
