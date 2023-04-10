ffd86d9aa997a3520fe8552c48d8e32beb54655c
Niharf14 <niharf14@gmail.com>
2023-04-10 15:50:43 +0530

    code updated

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
+import './Cart.css';
 
 const Cart = ({cart, handleRemoveFromCart}) => {
-    console.log(cart);
+    // console.log(cart);
+    let message;
+    if(cart.length === 0){
+        message = <p>Please add some products.</p>
+    }
+    else{
+        message = <div>
+            <h3>You are rich man</h3>
+            <p>Thank you!!</p>
+        </div>
+    }
     return (
         <div>
-            <h3>Order Summary: {cart.length}</h3>
+            <h3 className={cart.length > 0 ? 'yellow' : 'red'}>Order Summary: {cart.length}</h3>
+
+            <p className={`bold bordered ${cart.length > 4 ? 'goldenrod' : 'blue'}`}>Some info about Order</p>
+
+            {cart.length > 2 ? <span>Wow !! Rich man</span> : <span>Poor man</span>}
+
+            {message}
+
             {
                 cart.map(tshirt => <p 
                     key={tshirt._id}
                     >{tshirt.name} <button onClick={() => handleRemoveFromCart(tshirt._id)}>X</button></p>)
+            }
+
+            {
+                cart.length === 2 && <p>You bought two products only</p>
+            }
+
+            {
+                cart.length === 3 || <p>not three ok!!</p>
             }
         </div>
     );
